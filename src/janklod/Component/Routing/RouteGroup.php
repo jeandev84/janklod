fb737ebebfacdb426d1d2e9d8d922ccfa05a8a9e
Yao <jeanyao@ymail.com>
2021-02-19 18:13:01 +0300

    add route group

--- src/janklod/Component/Routing/RouteGroup.php
+++ src/janklod/Component/Routing/RouteGroup.php
@@ -12,45 +12,4 @@ use Closure;
 class RouteGroup
 {
 
-     /**
-      * @var array
-     */
-     protected $options = [];
-
-
-
-     /**
-      * @param array $options
-     */
-     public function setOptions(array $options)
-     {
-          $this->options = $options;
-     }
-
-
-     /**
-      * @param Closure $routeCallback
-      * @return false
-     */
-     public function call(Closure $routeCallback): bool
-     {
-          if(! empty($this->options))
-          {
-              $routeCallback();
-              $this->options = [];
-              return true;
-          }
-
-          return false;
-     }
-
-
-
-     /**
-      * @return array
-     */
-     public function getOptions(): array
-     {
-         return $this->options;
-     }
 }
