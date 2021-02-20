674a941978488f78461ebcf9eb9945467267617d
Yao <jeanyao@ymail.com>
2021-02-20 12:25:08 +0300

    testing route

--- public/demo/route.php
+++ public/demo/route.php
@@ -22,7 +22,6 @@ use Jan\Component\Routing\Router;
 
 $router = new Router();
 
-
 $options = [
   'prefix' => '/admin',
   'namespace' => 'Admin\\',
@@ -33,11 +32,13 @@ $options = [
   'name' => 'admin.'
 ];
 
+$router->pattern(['id' => '[0-9]+']);
+
 $router->group(function (Router $router) {
   $router->get('posts', 'PostController@index', 'list');
   $router->map('GET|POST', '/post/new', 'PostController@new', 'new');
   $router->map('GET|POST', '/post/{id}/edit', 'PostController@edit', 'edit');
-  $router->get('/post/{id}/delete', 'PostController@index', 'delete');
+  $router->get('/post/{id}/delete', 'PostController@delete', 'delete');
 
   $router->resource('/products', 'ProductController');
 
@@ -50,6 +51,17 @@ $router->resource('/cart', 'CartController');
 $router->get('/', 'HomeController@index', 'welcome');
 $router->get('/foo', 'FooController@index', 'foo');
 
-dd($router->getResources());
+
+dump($router->getRoutesByMethod(), $router->getGroups(), $router->getResources());
+
+if($route = $router->match($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI'])) {
+    echo "Matched route";
+    dd($route);
+}else{
+    echo "No matched route";
+    dd('Not Found!');
+}
+
+
 
 /* dd($router->getRoutes(), $router->getGroups(), $router->getResources()); */
