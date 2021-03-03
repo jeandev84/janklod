4ad3b5275da7750fccd3d511e250843c7397b152
Yao <jeanyao@ymail.com>
2021-03-03 18:09:08 +0300

    uppercase

--- views/site/partials/nav.php
+++ views/site/partials/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="/">
-        <?= app_name() ?>
+        <?= strtoupper(app_name()) ?>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
